8815cc511e3c1298611382fa6b003cd40f5e6920
vranjithreddy <33139072+vranjithreddy@users.noreply.github.com>
2018-02-15 16:33:22 -0500

    Update knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "ranjithrj2035"
+node_name                "####name"
 client_key               "#{current_dir}/ranjithrj2035.pem"
-chef_server_url          "https://api.chef.io/organizations/rjsample"
+chef_server_url          "https://api.chef.io/organizations/rj####"
 cookbook_path            ["#{current_dir}/../cookbooks"]
