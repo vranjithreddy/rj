0202643defac9032ecc5c51438e253900abb7c98
vranjithreddy <ranjith.raj2035@gmail.com>
2018-02-15 16:12:05 -0500

    Adding chef repo to git

---
+++ cookbooks/learn_chef_apache2/metadata.rb
@@ -0,0 +1,8 @@
+name 'learn_chef_apache2'
+maintainer 'Learn Chef'
+maintainer_email 'learnchef@chef.io'
+license 'Apache 2.0'
+description 'Installs and configures package apache2 and sets a basic home page.'
+long_description 'Installs and configures package apache2 and sets a basic home page. This cookbook is used by the Learn Chef tutorials. https://learn.chef.io'
+supports 'ubuntu'
+version '0.3.0'
